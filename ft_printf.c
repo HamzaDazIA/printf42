c17fd52f368f091661b579a93e2c9bc96dcd5244
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-11-22 17:18:26 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,81 +6,66 @@
 /*   By: hdazia <hdazia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 17:41:59 by hdazia            #+#    #+#             */
-/*   Updated: 2024/11/22 07:02:16 by hdazia           ###   ########.fr       */
+/*   Updated: 2024/11/22 17:16:18 by hdazia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-int fail_check(int i)
+int	fail_check(int	i)
 {
-    if (i < 0)
-        return (-1);
-    return (i);
+	if (i < 0)
+		return (-1);
+	return (i);
 }
-int ft_putstr(char *s)
+
+int	ft_check_conversions(const char	c, va_list	p)
 {
-	unsigned int	i;
+	int	how_mprint;
     
-	i = 0;
-    if (!s)
-    {
-        ft_putstr("(null)");
-        return (6);
-    }
-	while (s[i] != '\0')
-	{
-		ft_putchar(s[i]);
-		i++;
-	}
-    return (i);
+	how_mprint = 0;
+	if (c == 'c')
+		how_mprint += ft_putchar(va_arg(p, int));
+	else if (c == 's')
+		how_mprint += ft_putstr(va_arg(p, char *));
+	else if (c == 'd' || c == 'i')
+		how_mprint += ft_putnbr(va_arg(p, int));
+	else if (c == 'u')
+		how_mprint += ft_putnbr_unsignd(va_arg(p, unsigned int));
+	else if (c == '%')
+	   	how_mprint += ft_putchar('%');
+	else if (c == 'X' || c == 'x')
+		how_mprint += ft_printhex(va_arg(p, int), c);
+	else if (c == 'p')
+		how_mprint += ft_printp(va_arg(p, void *));
+	return (how_mprint);
 }
 
-int ft_check_conversions(const char c,va_list p)
-{
-    int  how_mprint;
-
-    how_mprint = 0;
-    if (c == 'c')
-        how_mprint += ft_putchar(va_arg(p,int));
-    else if (c == 's')
-        how_mprint += ft_putstr(va_arg(p,char *));
-    else if (c == 'd' || c == 'i')
-        how_mprint += ft_putnbr(va_arg(p,int));
-    else if (c == 'u')
-        how_mprint += ft_putnbr_unsignd(va_arg(p,unsigned int));
-    else if (c == '%')
-        how_mprint += ft_putchar('%');
-    else if (c == 'X' || c == 'x')
-        how_mprint += ft_printhex(va_arg(p,int),c);
-    return (how_mprint);
-}
-
-int ft_printf(const char * counst, ...)
+int	ft_printf(const char *counst, ...)
 {
     va_list p;
-    int  i;
-    int  how_mprint;
+	int     i;
+	int     how_mprint;
     
-    
-    how_mprint = 0;
-    i = 0;
-    va_start(p,counst);
-    while(counst[i] != '\0')
-    {
-        if (counst[i] == '%')
-        {
-            how_mprint += ft_check_conversions(counst[i + 1],p);
-            i++;
-        }
-        else
-        {
-            how_mprint += ft_putchar(counst[i]);
-        }
-        va_end(p);
-        i++;
-    }
-    how_mprint = fail_check(how_mprint);
-    return (how_mprint);
+    if (write(1, 0, 0) == -1)
+        return (-1);
+	how_mprint = 0;
+	i = 0;
+	va_start(p, counst);
+	while (counst[i] != '\0')
+	{
+		if (counst[i] == '%')
+		{
+			how_mprint += ft_check_conversions(counst[i + 1], p);
+			i++;
+		}
+		else
+		{
+			how_mprint += ft_putchar(counst[i]);
+		}
+		i++;
+	}
+	va_end(p);
+	how_mprint = fail_check(how_mprint);
+	return (how_mprint);
 }
- 
