114e9422861ae5e43aa6add00a70db315748bc8a
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-11-22 17:19:02 +0100

    Update and rename libftprintf.h to ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,18 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdint.h>
+
+int ft_printf(const char *counst, ...);
+int ft_putnbr(int n);
+int ft_putchar(char c);
+int ft_putnbr_unsignd(unsigned int n);
+int ft_printhex(unsigned int nb, char c);
+int fail_check(int i);
+int ft_putstr(char *s);
+int ft_printp(void *p);
+
+#endif
