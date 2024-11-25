6268ed76b4e3db0fef7886fa0fd952f4d0881364
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-11-25 12:56:33 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,10 +1,8 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdlib.h>
 # include <stdarg.h>
 # include <unistd.h>
-# include <stdint.h>
 
 int	ft_printf(const char *counst, ...);
 int	ft_putnbr(int n);
